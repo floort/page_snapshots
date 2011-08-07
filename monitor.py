fc13cc1787b604c229498c14dc27b0c18375a96f
Floor Terra <floort@gmail.com>
2011-08-08 01:45:11 +0200

    Automatic update

--- monitor.py
+++ monitor.py
@@ -30,6 +30,7 @@ MONITOR = (
     ("https://www.dropbox.com/terms", "files/dropbox_en.html"),
     ("http://www.skype.com/intl/nl/legal/privacy/general/", "files/skype_nl.html"),
     ("https://twitter.com/privacy", "files/twitter_privacy.html"),
+    ("http://www.facebook.com/policy.php?_fb_noscript=1", "files/facebook_privacy.html"),
 )
 
 # Get all the pages
