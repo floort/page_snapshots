c5c8a36d15c6a67b0f1b15e4e57d3a27b7c62d31
Floor Terra <floort@gmail.com>
2011-08-08 13:39:19 +0200

    Automatic update

--- monitor.py
+++ monitor.py
@@ -40,10 +40,19 @@ MONITOR = (
     ("https://www.sidn.nl/fileadmin/docs/PDF-files_NL/Algemene_voorwaarden_voor_registrars.pdf", "files/sidn_algemene_voorwaarder_registrars.pdf"),
     ("https://www.sidn.nl/fileadmin/docs/PDF-files_NL/Algemene_voorwaarden_voor_affiliates.pdf", "files/sidn_algemene_voorwaarder_affiliates.pdf"),
     ("https://www.sidn.nl/fileadmin/docs/PDF-files_NL/Technische%20eisen%20voor%20registratie%20van%20.nl-domeinnamen.pdf", "files/sidn_technische_eisen.pdf"),
+    ("http://www.hi.nl/Privacy.htm", "files/hi_privacy.html"),
+    ("https://mtvmobile.nl/privacystatement", "files/mtvmobile_privacy.html"),
+    ("http://www.hyves.nl/privacy/", "files/hyves_privacy.html"),
+    ("http://www.rabomobiel.nl/rabomobiel/footer/privacy", "files/rabomobiel_privacy.html"),
+    ("http://overupc.upc.nl/disclaimer-privacy/", "files/upc_privacy.html"),
+    ("http://www.online.nl/voorwaarden/privacy-statement/", "files/online_privacy.html"),
+    ("http://www.sizz.nl/privacy/", "files/sizz_privacy.html"),
+    ("http://static.ziggo.nl:443/images/Privacybeleid%20Ziggo_tcm14-2127.pdf", "files/ziggo_privacy.pdf"),
 )
 
 # Get all the pages
 for page in MONITOR:
+    print page[0]
     page_content = urllib2.urlopen(page[0]).read()
     open(page[1], "w").write(page_content)
 
